3ad7f74f1bb397dbf89b22062a1a84d2846e92df
GARENI SAITEJA <2320030023@klh.edu.in>
2025-10-22 11:09:05 +0530

    Add Terraform configuration for Docker Nginx setup

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx_test" {
+  name  = "cicd-nginx"
+  image = docker_image.nginx.latest
+  ports {
+    internal = 80
+    external = 8082
+  }
+}
+
+output "container_ports" {
+  value = [{
+    name = docker_container.nginx_test.name
+    host_port = 8082
+  }]
+}
